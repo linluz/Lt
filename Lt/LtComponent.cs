619395c73b3d7b7be933c65c47b6f0cd49b6c966
luche lin <l329978214@gmail.com>
2019-10-20 13:48:16 +0800

    添加【高程分析】和【坡度分析】

--- Lt/LtComponent.cs
+++ Lt/LtComponent.cs
@@ -12,23 +12,23 @@ using Rhino.Geometry;
 
 namespace Lt.Analysis
 {/// <summary>
-///  电池界面 添加渐变范围、添加图标、关闭预览中的网格线部分
-/// </summary>
-    public class LTFT : GH_Component
+ ///  电池界面 添加渐变范围、关闭预览中的网格线部分
+ /// </summary>
+ public class LTFT : GH_Component
     {
         public LTFT()
           : base("地形网格淹没分析", "LTFT","分析被水淹没后的地形状态","Lt", "分析")
         {}//Flooded Terrain
         protected override void RegisterInputParams(GH_InputParamManager pManager)
         {
-            pManager.AddMeshParameter("地形", "Tm", "要被淹没的山地地形网格", GH_ParamAccess.item);
+            pManager.AddMeshParameter("地形", "Mt", "要被淹没的山地地形网格", GH_ParamAccess.item);
             pManager.AddNumberParameter("高度", "E", "淹没地形的水平面高度", GH_ParamAccess.item);
             pManager.AddColourParameter("色彩", "C", "被水淹没区域的色彩", GH_ParamAccess.item,Color.FromArgb(52,58,107));
         }
         
         protected override void RegisterOutputParams(GH_OutputParamManager pManager)
         {
-            pManager.AddMeshParameter("地形", "Fm", "被水淹没后的地形网格", GH_ParamAccess.item);
+            pManager.AddMeshParameter("地形", "Mf", "被水淹没后的地形网格", GH_ParamAccess.item);
         }
         protected override void SolveInstance(IGH_DataAccess DA)
         {
@@ -83,16 +83,16 @@ namespace Lt.Analysis
                 Color.FromArgb(254,244,84),
                 Color.FromArgb(234,126,0),
                 Color.FromArgb(219,37,0),
-                Color.FromArgb(138,36,36),
+                Color.FromArgb(138,36,36)
             }
         );
         protected override Bitmap Icon => LTResource.山体淹没分析;
         public override Guid ComponentGuid => new Guid("84474303-59cb-4248-9015-c5a02098fd99");
     }
-public class LTSD : GH_Component
+ public class LTSD : GH_Component
 {
     public LTSD()
-        : base("坡向分析", "LTFT", "山坡地形朝向分析,X轴向为东,Y轴向为北\r\n双击电池图标以切换着色模式", "Lt", "分析")
+        : base("坡向分析", "LTSD", "山坡地形朝向分析,X轴向为东,Y轴向为北\r\n双击电池图标以切换着色模式", "Lt", "分析")
     {
         Num1 = Math.Tan(Math.PI / 8);
         Num2 = Math.Tan(Math.PI*3 / 8);
@@ -101,7 +101,7 @@ public class LTSD : GH_Component
         }
     protected override void RegisterInputParams(GH_InputParamManager pManager)
     {
-        pManager.AddMeshParameter("地形", "Tm", "要进行坡向分析的山地地形网格", GH_ParamAccess.item);
+        pManager.AddMeshParameter("地形", "Mt", "要进行坡向分析的山地地形网格", GH_ParamAccess.item);
         pManager.AddColourParameter("色彩", "C", "各向的色彩，请按上、北、东北、东、东南、南、西南、西、西北的顺序连入9个色彩", GH_ParamAccess.list,
             new List<Color>
             {
@@ -118,7 +118,7 @@ public class LTSD : GH_Component
         }
     protected override void RegisterOutputParams(GH_OutputParamManager pManager)
     {
-        pManager.AddMeshParameter("地形", "Cm", "已根据坡向着色的地形网格", GH_ParamAccess.item);
+        pManager.AddMeshParameter("地形", "Mc", "已根据坡向着色的地形网格", GH_ParamAccess.item);
     }
     protected override void SolveInstance(IGH_DataAccess DA)
     {
@@ -129,8 +129,7 @@ public class LTSD : GH_Component
         List<Color> c = new List<Color>(9);
         if (!DA.GetDataList(1, c))
             return;
-            #endregion
-
+        #endregion
             Mesh t1 = new Mesh();
             if (Shade)
             {
@@ -197,7 +196,7 @@ public class LTSD : GH_Component
     protected override Bitmap Icon => LTResource.山体坡向分析;
     public override Guid ComponentGuid => new Guid("3d3ee5a9-c86e-4007-97c6-eb33aa365e27");
     }
-public class LTSD_Attributes : GH_ComponentAttributes
+ public class LTSD_Attributes : GH_ComponentAttributes
 {
     public LTSD_Attributes(IGH_Component component) : base(component)
     { }
@@ -212,4 +211,88 @@ public class LTSD_Attributes : GH_ComponentAttributes
         return GH_ObjectResponse.Handled;
     }
 }
+ public class LTTG : GH_Component
+ {
+     public LTTG() : base("坡度分析", "LTTG", "山地地形坡度分析", "Lt", "分析")
+     {}//Terrain Grade
+     protected override void RegisterInputParams(GH_InputParamManager pManager)
+     {
+         pManager.AddMeshParameter("地形", "Mt", "要进行坡度分析的山地地形网格", GH_ParamAccess.item);
+     }
+     protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+     {
+         pManager.AddMeshParameter("地形", "M", "已按角度着色的地形网格", GH_ParamAccess.item);
+         pManager.AddIntervalParameter("角度", "A", "坡度范围（度）", GH_ParamAccess.item);
+     }
+
+     protected override void SolveInstance(IGH_DataAccess DA)
+     {
+         Mesh tm=new Mesh();
+         if (!DA.GetData(0,ref tm))
+             return;
+         List<double> ra = new List<double>(3);
+         Interval ia = new Interval();
+         foreach (Vector3f v in tm.Normals)
+         {
+             double a = Math.Acos(v.Z) * 180/Math.PI;
+             ia.Grow(a);
+             ra.Add(a);
+         }
+         Mesh cm = tm.DuplicateMesh();
+         foreach (double a in ra)
+             cm.VertexColors.Add(gradient.ColourAt(ia.NormalizedParameterAt(a)));
+         DA.SetData(0, cm);
+         DA.SetData(1, ia);
+        }
+     public static  GH_Gradient gradient = new GH_Gradient(
+         new[] { 0, 0.2, 0.4, 0.6, 0.8, 1 },
+         new[]
+         {
+             Color.FromArgb(45,51,87),
+             Color.FromArgb(75 ,107,169),
+             Color.FromArgb(173,203,249),
+             Color.FromArgb(254,244,84),
+             Color.FromArgb(234,126,0),
+             Color.FromArgb(237,53,17)
+         }
+     );
+     protected override Bitmap Icon => LTResource.山体坡度分析;
+     public override Guid ComponentGuid => new Guid("6c33fb8b-9da6-4688-8a1b-d0363350d176");
+    }
+ public class LTTE : GH_Component
+ {
+     public LTTE() : base("高程分析", "LTTE", "山地地形高程分析", "Lt", "分析")
+     { }//Terrain Grade
+     protected override void RegisterInputParams(GH_InputParamManager pManager)
+     {
+         pManager.AddMeshParameter("地形", "Mt", "要进行坡度分析的山地地形网格", GH_ParamAccess.item);
+     }
+     protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+     {
+         pManager.AddMeshParameter("地形", "M", "已按海拔着色的地形网格", GH_ParamAccess.item);
+         pManager.AddIntervalParameter("海拔", "E", "海拔范围（两位小数）", GH_ParamAccess.item);
+     }
+
+     protected override void SolveInstance(IGH_DataAccess DA)
+     {
+         Mesh tm = new Mesh();
+         if (!DA.GetData(0, ref tm))
+             return;
+         List<double> re = new List<double>(3);
+         Interval ie = new Interval();
+         foreach (Point3f p in tm.Vertices)
+         {
+             double e = Math.Round(p.Z,2);
+             ie.Grow(e);
+             re.Add(e);
+         }
+         Mesh cm = tm.DuplicateMesh();
+         foreach (double e in re)
+             cm.VertexColors.Add(LTTG.gradient.ColourAt(ie.NormalizedParameterAt(e)));
+         DA.SetData(0, cm);
+         DA.SetData(1, ie);
+     }
+     protected override Bitmap Icon => LTResource.山体高程分析;
+     public override Guid ComponentGuid => new Guid("1afc0549-5308-47ef-b91c-a2eade694250");
+ }
 }
