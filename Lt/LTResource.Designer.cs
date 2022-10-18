2f8f422bf05c649218edf4cb9a92f1a9ef506494
luche lin <l329978214@gmail.com>
2022-10-18 15:48:07 +0800

    新增图片，用于【设置颜色】菜单项的图标

--- Lt/LTResource.Designer.cs
+++ Lt/LTResource.Designer.cs
@@ -133,6 +133,16 @@ namespace Lt {
         /// <summary>
         ///   查找 System.Drawing.Bitmap 类型的本地化资源。
         /// </summary>
+        internal static System.Drawing.Bitmap Sprite_20x20 {
+            get {
+                object obj = ResourceManager.GetObject("Sprite_20x20", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   查找 System.Drawing.Bitmap 类型的本地化资源。
+        /// </summary>
         internal static System.Drawing.Bitmap 云线 {
             get {
                 object obj = ResourceManager.GetObject("云线", resourceCulture);
