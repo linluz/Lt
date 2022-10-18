a1ee41a92f01d4fea40dc9104157b6ef09b0bd62
luche lin <l329978214@gmail.com>
2022-10-18 14:34:29 +0800

    版本修改为0.3

--- Lt/LtInfo.cs
+++ Lt/LtInfo.cs
@@ -18,14 +18,10 @@ namespace Lt
         public override string AuthorName => "兰亭 & 林师兄";
 
         public override string AuthorContact => "1142060440@qq.com & 329978214@qq.com";
-        public override string Version => "0.0.2";
+        public override string Version => "0.3";
     }
     public class AssemblyPriority : GH_AssemblyPriority
     {
-        // Token: 0x060002BE RID: 702 RVA: 0x00003A7B File Offset: 0x00001C7B
-        public AssemblyPriority()
-        { }
-        // Token: 0x060002BF RID: 703 RVA: 0x0001310C File Offset: 0x0001130C
         public override GH_LoadingInstruction PriorityLoad()
         {
             Instances.ComponentServer.AddCategoryIcon("Lt", LTResource.ltlogo16);
