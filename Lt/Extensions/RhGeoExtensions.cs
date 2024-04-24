0c43ea108436f2a7bf635219f92acdfd4be0c74c
luche lin <l329978214@gmail.com>
2024-04-24 20:04:58 +0800

    增加扩展方法

---
+++ Lt/Extensions/RhGeoExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Rhino.Geometry;
+
+namespace Lt.Extensions
+{
+    public static class RhGeoExtensions
+    {
+        public static BoundingBox UnionPoint(this BoundingBox box, Point3d point)
+        {
+            box.Union(point);
+            return box;
+        }
+    }
+}
---
+++ Lt/Extensions/StringExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace Lt.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool IsNullOrEmpty(this string str)
+            => string.IsNullOrEmpty(str);
+        public static bool NotNullOrEmpty(this string str)
+            => !string.IsNullOrEmpty(str);
+        public static bool IsNullOrWhiteSpace(this string str)
+            => string.IsNullOrWhiteSpace(str);
+        public static bool NotNullOrWhiteSpace(this string str)
+            => !string.IsNullOrWhiteSpace(str);
+    }
+}
