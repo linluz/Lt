2d07c9f3f67ea1109a9871c8ff157e41ded9587c
luche lin <l329978214@gmail.com>
2024-05-07 01:18:00 +0800

    对应增加Majas：增加类【RhMethodInfo】【Method】用于根据版本不同，自动调用不同的方法；增加扩展方法【CreateBooleanUnion】,对应调整【CanopyC】的代码

--- Lt/GHComponent/Basis/CanopyC.cs
+++ Lt/GHComponent/Basis/CanopyC.cs
@@ -15,7 +15,6 @@ namespace Lt.GHComponent.Basis;
 // ReSharper disable once UnusedMember.Global
 public class CanopyC : ADCComponent
 {
-    //debug 待 及双击效果
     public CanopyC() : base(
         "林冠线", "LTCanopyC",
         "通过轮廓曲线来生成林冠线",
@@ -37,19 +36,18 @@ public class CanopyC : ADCComponent
     protected override void SolveInstance(IGH_DataAccess DA)
     {
         #region 初始化 获取输入
-        if (!DA.OutData(0, out Curve b, new PolyCurve()) || RMNoValid(b, 0) || RMNoClosed(b, 0) || RNNoPlanar(b, 0) || //获取线框，并检测闭合与平面
+        if (!DA.OutData(0, out Curve b, new PolyCurve()) || RMNoValid(b, 0) || RMNoClosed(b, 0) || RNNoPlanar(b, 0, out Plane plane) || //获取线框，并检测闭合与平面
             !DA.OutData(1, out double r) || RMSmaller(r, 0, 1, equal: true) ||
             !DA.OutData(2, out double d) || RMSmaller(d, 0, 2, equal: true) ||
             !DA.OutData(3, out double l) || RMSmaller(l, 0, 3, equal: true) ||
             !DA.OutData(4, out int s)) return;
         #endregion
-        //bug 代码已正确 但运行效率是是原电池的7倍，待查
+
         var TreeArea = r * r * Math.PI;//树冠面积
         var TreeArea2 = TreeArea * d;//每颗树所占的树冠面积
         Curve[] bs = [b];
         if (Restrict.Def)
         {
-            b.TryGetPlane(out Plane plane);
             bs = b.Offset(plane, -r, DocumentTolerance(), CurveOffsetCornerStyle.Sharp) //偏移出树心限制线
                 .Where(t => t is { IsValid: true, IsClosed: true })//剔除为null或无效或不闭合的线
                 .ToArray();
@@ -60,19 +58,14 @@ public class CanopyC : ADCComponent
             {
                 Brep sur = Brep.CreatePlanarBreps(c)[0];
                 var count = (int)(sur.GetArea() / TreeArea2);//树量=面积/(树冠面积/单冠密度)
-                return new BrepPopulation(sur, s).Populate(count, null)//获取随机点
+                return new BrepPopulation(sur, s).Populate(count, null) //获取随机点
                     .Where(p => b.Contains(p) is PointContainment.Coincident or PointContainment.Inside) //保留内部或在边界上的点
                     .Select(p => new Circle(p, r).ToNurbsCurve()) //转换为圆
-                    .ToArray();
+                    .CreateBooleanUnion(DocumentTolerance()) /// //转成圆组并求交集
+                    .Where(t0 => t0.GetLength() >= l) //剔除长度短于l
+                    .Select(t0 => new GH_Curve(t0));
             })
-            .Select(t =>
-                Curve.CreateBooleanUnion(t) //转成圆组并求交集
-                    .Where(t0 => t0.GetLength() >= l)//剔除长度短于l
-                    .Select(t0 => new GH_Curve(t0))
-            )
-            .SelectMany(t => t)
-            .ToArray();
-
+            .SelectMany(t => t);
         group.Objects.AddRange(ra);
 
         DA.SetData(0, group);
