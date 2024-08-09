08c2bd12b7eba8a47844a59d0459c0fd66b0a80c
luche lin <l329978214@gmail.com>
2024-08-09 10:40:22 +0800

    增加字符串扩展方法

--- Lt/Base/Extensions/StringExtensions.cs
+++ Lt/Base/Extensions/StringExtensions.cs
@@ -1,13 +1,60 @@
-﻿namespace Lt.Base.Extensions;
+﻿#nullable enable
+using System.IO;
+using System.Text;
 
-public static class StringExtensions
+namespace Lt.Base.Extensions;
+
+/// <summary>
+/// 扩展方法
+/// </summary>
+public static class StringExtension
 {
-    public static bool IsNullOrEmpty(this string str)
-        => string.IsNullOrEmpty(str);
-    public static bool NotNullOrEmpty(this string str)
-        => !string.IsNullOrEmpty(str);
-    public static bool IsNullOrWhiteSpace(this string str)
-        => string.IsNullOrWhiteSpace(str);
-    public static bool NotNullOrWhiteSpace(this string str)
-        => !string.IsNullOrWhiteSpace(str);
+    public static bool IsNullOrEmpty(this string? s)
+        => string.IsNullOrEmpty(s);
+    public static bool NotNullOrEmpty(this string? s)
+        => !string.IsNullOrEmpty(s);
+    public static bool IsNullOrWhiteSpace(this string? s)
+        => string.IsNullOrWhiteSpace(s);
+    public static bool NotNullOrWhiteSpace(this string? s)
+        => !string.IsNullOrWhiteSpace(s);
+    public static bool FileExists(this string s)
+        => File.Exists(s);
+    public static bool FileNotExists(this string s)
+        => !File.Exists(s);
+    public static bool DirExists(this string s)
+        => Directory.Exists(s);
+    public static bool DirNotExists(this string s)
+        => !Directory.Exists(s);
+    /// <summary>
+    /// 字符转码
+    /// </summary>
+    /// <param name="s"></param>
+    /// <returns></returns>
+    public static byte[] ToUtf8Byte(this string s)
+        => Encoding.UTF8.GetBytes(s);
+
+    /// <summary>
+    /// 码转字符
+    /// </summary>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    public static string ToUtf8String(this byte[] b)
+        => Encoding.UTF8.GetString(b);
+
+    /// <summary>
+    /// 给输入的本机路径或者远程路径增加长路径前缀
+    /// </summary>
+    /// <param name="path"></param>
+    /// <param name="limit">是否判断长度，True则判断长度，超过250才加前缀,否则一定加前缀，默认为True</param>
+    /// <returns></returns>
+    public static string ToLongPath(this string path, bool limit = true)
+    {
+        if (path.IsNullOrEmpty()) return path;
+        if (path.StartsWith(@"\\?\")) return path;
+        return limit && path.Length < 255
+            ? path
+            : path.StartsWith(@"\\")
+                ? @"\\?\UNC\" + path.Substring(2)
+                : @"\\?\" + path;
+    }
 }
